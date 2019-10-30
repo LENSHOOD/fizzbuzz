75ee010a4ad853ce6b04cb813070eb52617868f0
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-10-30 22:20:50 +0800

    feat: deal with user input and output

--- src/main/java/zxh/demo/fizzbuzz/FizzBuzz.java
+++ src/main/java/zxh/demo/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,26 @@
 package zxh.demo.fizzbuzz;
 
+import java.util.stream.IntStream;
+
 /**
  * FizzBuzz:
  * @author zhangxuhai
  * @date 2019/10/30
 */
 public class FizzBuzz {
+
+    private static final int MIN = 1;
+    private static final int MAX = 50;
+
+    public String process(int from, int to) {
+        if (from < MIN || to > MAX) {
+            throw new FizzBuzzException("The valid input range is 1 to 50");
+        }
+
+        StringBuilder result = new StringBuilder();
+        IntStream.range(from, to + 1).forEach(i -> result.append(calculate(i)).append(" "));
+        return result.toString();
+    }
 
     public String calculate(int i) {
         if (isFizzBuzz(i)) {
---
+++ src/main/java/zxh/demo/fizzbuzz/FizzBuzzException.java
@@ -0,0 +1,12 @@
+package zxh.demo.fizzbuzz;
+
+/**
+ * FizzBuzzException:
+ * @author zhangxuhai
+ * @date 2019/10/30
+*/
+public class FizzBuzzException extends RuntimeException{
+    public FizzBuzzException(String message) {
+        super(message);
+    }
+}
